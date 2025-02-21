7f98d34748aae0202cb1ba16b1a34b19e6442eab
Cristhian Andres Moreno Velasco <Cristhian.452@outlook.com>
2025-02-21 14:44:14 -0500

    Estructura de Paquetería definida

--- src/main/java/com/plaza/usuarios/UsuariosApplication.java
+++ src/main/java/com/plaza/usuarios/UsuariosApplication.java
@@ -3,11 +3,13 @@ package com.plaza.usuarios;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
+
 public class UsuariosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UsuariosApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(UsuariosApplication.class, args);
 	}
 
 }
